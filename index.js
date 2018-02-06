ef5514bb344991c3671e6b9cf761204d7cff3269
Sindre Sorhus <sindresorhus@gmail.com>
2018-02-06 16:08:21 +0700

    Improvements based on actual usage

--- index.js
+++ index.js
@@ -9,9 +9,17 @@ module.exports = {
 	],
 	rules: {
 		'at-rule-no-unknown': null,
+		'at-rule-empty-line-before': null,
 		'scss/at-else-if-parentheses-space-before': 'always',
 		'scss/at-extend-no-missing-placeholder': true,
-		'scss/at-function-named-arguments': 'always',
+		'scss/at-function-named-arguments': [
+			'always',
+			{
+				ignore: [
+					'single-argument'
+				]
+			}
+		],
 		'scss/at-function-parentheses-space-before': 'never',
 		'scss/at-function-pattern': reLowercase,
 		'scss/at-import-no-partial-leading-underscore': true,
@@ -23,17 +31,17 @@ module.exports = {
 		'scss/at-mixin-parentheses-space-before': 'never',
 		'scss/at-mixin-pattern': reLowercase,
 		'scss/at-rule-no-unknown': true,
-		'scss/dollar-variable-colon-newline-after': 'always-multi-line',
+		// Disabled because of https://github.com/kristerkari/stylelint-scss/issues/203
+		// 'scss/dollar-variable-colon-newline-after': 'always-multi-line',
 		'scss/dollar-variable-colon-space-after': 'always',
 		'scss/dollar-variable-colon-space-before': 'never',
 		'scss/dollar-variable-no-missing-interpolation': true,
 		'scss/dollar-variable-pattern': reLowercase,
 		'scss/percent-placeholder-pattern': reLowercase,
-		'scss/double-slash-comment-whitespace-inside': 'always',
+		// Disabled because of https://github.com/kristerkari/stylelint-scss/issues/202
+		// 'scss/double-slash-comment-whitespace-inside': 'always',
 		'scss/declaration-nested-properties': 'never',
 		'scss/declaration-nested-properties-no-divided-groups': true,
-		'scss/operator-no-newline-after': true,
-		'scss/operator-no-newline-before': true,
 		'scss/operator-no-unspaced': true,
 		'scss/partial-no-import': true,
 		'scss/selector-no-redundant-nesting-selector': true
