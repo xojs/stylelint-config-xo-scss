1acdcc8bd746a6ed1b6142360d1cc1df997ad7bf
Sindre Sorhus <sindresorhus@gmail.com>
2019-12-01 17:11:25 +0700

    Update dependencies

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@ import test from 'ava';
 import stylelint from 'stylelint';
 import config from '..';
 
+const hasRule = (errors, ruleId) => errors.some(x => x.rule === ruleId);
+
 const runStylelint = async code => {
 	const data = await stylelint.lint({
 		code,
@@ -12,10 +14,12 @@ const runStylelint = async code => {
 };
 
 test('main', async t => {
-	const results = await runStylelint(`div {
-	left: .2em;
-}
-`);
+	const results = await runStylelint(
+		`div {
+			left: .2em;
+		}
+		`
+	);
 
-	t.is(results[0].warnings[1].rule, 'number-leading-zero');
+	t.true(hasRule(results[0].warnings, 'number-leading-zero'));
 });
