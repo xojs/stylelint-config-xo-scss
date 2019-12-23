517a558531182bc5b1d00616ec80c3628509ca66
Sindre Sorhus <sindresorhus@gmail.com>
2019-12-23 14:33:34 +0100

    Fix the value for the `no-duplicate-dollar-variables` rule
    Fixes #1

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ module.exports = {
 		'scss/at-function-pattern': reLowercase,
 		'scss/at-if-no-null': true,
 		'scss/at-import-no-partial-leading-underscore': true,
-		'scss/at-import-partial-extension': true,
+		'scss/at-import-partial-extension': 'always',
 		'scss/at-import-partial-extension-blacklist': [
 			'scss'
 		],
