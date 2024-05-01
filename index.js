c955302717d85efcb15070f222b4e2a44d7aa901
Sindre Sorhus <sindresorhus@gmail.com>
2024-05-01 19:19:46 +0700

    Require Node.js 18, Stylelint 16, and move to ESM

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-'use strict';
+import postcssScss from 'postcss-scss';
 
 const reLowercase = /^[a-z]+(-[a-z\d]+)*$/;
 
-module.exports = {
+const config = {
 	extends: 'stylelint-config-xo',
-	customSyntax: require('postcss-scss'),
+	customSyntax: postcssScss,
 	plugins: [
-		'stylelint-scss'
+		'stylelint-scss',
 	],
 	rules: {
 		'at-rule-no-unknown': null,
@@ -15,10 +15,11 @@ module.exports = {
 			true,
 			{
 				ignoreAtRules: [
-					'use'
-				]
-			}
+					'use',
+				],
+			},
 		],
+		'property-no-unknown': null,
 
 		'scss/at-each-key-value-single-line': true,
 		'scss/at-else-if-parentheses-space-before': 'always',
@@ -26,15 +27,15 @@ module.exports = {
 		'scss/at-function-parentheses-space-before': 'never',
 		'scss/at-function-pattern': reLowercase,
 		'scss/at-if-no-null': true,
-		'scss/at-import-no-partial-leading-underscore': true,
 		'scss/at-import-partial-extension': 'never',
 		'scss/at-mixin-argumentless-call-parentheses': 'never',
 		'scss/at-mixin-parentheses-space-before': 'never',
 		'scss/at-mixin-pattern': reLowercase,
+		'scss/at-root-no-redundant': true,
 		'scss/at-rule-conditional-no-parentheses': true,
 		'scss/at-rule-no-unknown': true,
 		'scss/at-use-no-unnamespaced': true,
-		// Disabled because of https://github.com/kristerkari/stylelint-scss/issues/203
+		// Disabled because of https://github.com/stylelint-scss/stylelint-scss/issues/172#issuecomment-363594714
 		// 'scss/dollar-variable-colon-newline-after': 'always-multi-line',
 		'scss/dollar-variable-colon-space-after': 'always-single-line',
 		'scss/dollar-variable-colon-space-before': 'never',
@@ -56,6 +57,14 @@ module.exports = {
 		'scss/partial-no-import': true,
 		'scss/selector-no-redundant-nesting-selector': true,
 		'scss/no-duplicate-dollar-variables': true,
-		'scss/no-duplicate-mixins': true
-	}
+		'scss/no-duplicate-mixins': true,
+		'scss/property-no-unknown': true,
+		'scss/at-use-no-redundant-alias': true,
+		'scss/function-calculation-no-interpolation': true,
+		'scss/block-no-redundant-nesting': true,
+		'scss/load-no-partial-leading-underscore': true,
+		'scss/no-unused-private-members': true,
+	},
 };
+
+export default config;
